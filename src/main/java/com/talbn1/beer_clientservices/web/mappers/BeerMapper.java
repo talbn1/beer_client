48f61733cd34cd1aed7c71a3aac73e95c6864c14
talbn1 <talbn1@gmail.com>
2020-06-29 10:18:43 +0300

    Added DateMapper.java and connect it to BeerMapper.java

---
+++ src/main/java/com/talbn1/beer_clientservices/web/mappers/BeerMapper.java
@@ -0,0 +1,17 @@
+package com.talbn1.beer_clientservices.web.mappers;
+
+import com.talbn1.beer_clientservices.domain.Beer;
+import com.talbn1.beer_clientservices.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * @author talbn on 6/29/2020
+ **/
+
+
+//Extened the datemapper use in here
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    BeerDto beerToBeerDto(Beer beer);
+    Beer BeerDtoToBeer(BeerDto beerDto);
+}
